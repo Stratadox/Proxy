2697a147c06490f9c5ee0ffad56b47e0d09f2bd8
Stratadox <jesse@stratadox.com>
2018-02-25 17:16:18 +0100

    Throw an informative exception when no proxy loader is available

--- src/Proxying.php
+++ src/Proxying.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Stratadox\Proxy;
 
+use function get_class as classOf;
 use function is_null as weDidNotYetLoad;
+use LogicException;
+use function sprintf as withMessage;
 
 /**
  * Lazily loads proxy targets.
@@ -23,6 +26,12 @@ trait Proxying
     /** @return self */
     public function __load()
     {
+        if (!$this->loader instanceof LoadsProxiedObjects) {
+            throw new LogicException(withMessage(
+                'Cannot load the proxy without a loader. Proxy class: %s',
+                classOf($this)
+            ));
+        }
         if (weDidNotYetLoad($this->instance)) {
             /** @var Proxy $this */
             $this->instance = $this->loader->loadTheInstance();
